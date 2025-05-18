ffa0e8ee131d8edc9fd938bea0f11f01a19a18e6
Sevmamra <sevmamra01@gmail.com>
2025-05-18 09:42:33 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", "12345"))
-API_HASH = os.environ.get("API_HASH", "your_api_hash_here")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token_here")
-MONGO_URL = os.environ.get("MONGO_URL", "your_mongo_url_here")
-OWNER_ID = int(os.environ.get("OWNER_ID", "8734782482"))
+API_ID = int(os.environ.get("API_ID", "26797881"))
+API_HASH = os.environ.get("API_HASH", "9699262c708c2e45ba18bfce925ed5ed")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8036885246:AAGx0sS5TY8xV2Vl8viIqaDTIoL4wpbYhCI")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://sevmamra01:<db_password>@cluster0.ff29e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = int(os.environ.get("OWNER_ID", "6567162029"))
